72595751645b057c7b3860bc59f74fc70c8999cd
Jason Chan <jasonchanhw0531@gmail.com>
2024-05-01 16:37:46 +0100

    returned header test

--- portfolio-page/src/components/Header.tsx
+++ portfolio-page/src/components/Header.tsx
@@ -4,7 +4,6 @@ import HeaderItem from './HeaderItem'
 export default function Header(){
     return(
         <div className="header header-animation">
-            <HeaderItem>NO</HeaderItem>
             <HeaderItem>About</HeaderItem>
             <HeaderItem>Education </HeaderItem>
             <HeaderItem>Projects/Experience</HeaderItem>
