df8563d383b06c5293f785211f0890e007ad8569
Jason Chan <jasonchanhw0531@gmail.com>
2024-05-01 21:56:52 +0100

    added svgs and functionality to about section, changed some about section animations

--- portfolio-page/src/components/About.tsx
+++ portfolio-page/src/components/About.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 import face from "../photos/face.png"
+import { LogoLinkedin,LogoGithub,LogoGmail} from "./Logos";
 
 export default function About(){
+    
+    const[clipboardText,setClipboardText] = React.useState("Copy to clipboard")
+
+    function onClick():void{
+        navigator.clipboard.writeText("jasonchanhw0531@gmail.com")
+        setClipboardText("Email copied")
+    }
+
+    function onmouseover(){
+        setClipboardText("Copy to clipboard")
+    }
+
     return(
         <div className="about">
             <div className="about-item">
@@ -11,6 +24,18 @@ export default function About(){
                     Self-taught in coding, namely Python (Django, Flask) and JavaScript (React, TypeScript) as well as HTML, CSS. 
                     Currently producing personal projects to expand and develop my knowledge. 
                     Highly motivated, adaptable, and dedicated to personal and professional development. 
+                </div>
+                <div className="about-svg">
+                    <a href="https://www.linkedin.com/in/ho-wa-chan-b207a0213" target="_blank">
+                        <LogoLinkedin />
+                    </a>
+                    <a href="https://www.github.com/JC33340" target="_blank">
+                        <LogoGithub />
+                    </a>
+                    <div className="about-svg-gmail">
+                        <LogoGmail onclick = {onClick} onmouseover={onmouseover} />
+                        <div>{clipboardText}</div>
+                    </div>
                 </div>
             </div>
             <div className="about-item">
