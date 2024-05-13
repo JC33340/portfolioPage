f1d9c5b13372e46b888abcf937b6d00aa3976d74
Jason Chan <jasonchanhw0531@gmail.com>
2024-05-13 19:42:22 +0100

    removed education section from header

--- portfolio-page/src/components/Header.tsx
+++ portfolio-page/src/components/Header.tsx
@@ -7,7 +7,6 @@ export default function Header(){
         <div className="header header-animation">
             <div className='header-left'>
                 <HeaderItem>About</HeaderItem>
-                <HeaderItem>Education </HeaderItem>
                 <HeaderItem>Projects</HeaderItem>
             </div>
             
