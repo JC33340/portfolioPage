a807a71c19565eff9a3d50c94cb3064d81f6c93d
Jason Chan <jasonchanhw0531@gmail.com>
2024-05-15 05:53:00 +0100

    fixed more use intersection observer

--- portfolio-page/src/customHooks/useIntersectionObserver.tsx
+++ portfolio-page/src/customHooks/useIntersectionObserver.tsx
@@ -17,7 +17,7 @@ export default function useIntersectionObserver(threshold:number):[boolean,Mutab
 
     React.useEffect(()=>{
         const observer = new IntersectionObserver(callBack,options)
-        let ref = null
+        let ref:MutableRefObject<null>|HTMLElement|null = null
         if (containerRef.current){
             observer.observe(containerRef.current)
             ref = containerRef.current
