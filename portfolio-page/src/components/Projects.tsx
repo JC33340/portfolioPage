0a2f1e3d6197c63530191d669c3e7a3745f2e093
Jason Chan <jasonchanhw0531@gmail.com>
2024-10-12 14:36:00 +0100

    changed CV builder to new

--- portfolio-page/src/components/Projects.tsx
+++ portfolio-page/src/components/Projects.tsx
@@ -24,7 +24,7 @@ export default function Projects(){
         technologiesUsed:string[],
         Link?:string
     }[] =[
-        {name:"CV-Builder",description:"Converts user input into a downloadable PDF",pictures:[cvBuilder1,cvBuilder2],youtubeLink:undefined,githubLink:"https://github.com/JC33340/CVbuilder.git",technologiesUsed:["react","HTML","CSS"], Link:'https://cvbuilder-d69.pages.dev/'},
+        {name:"CV-Builder",description:"Converts user input into a downloadable PDF",pictures:[cvBuilder1,cvBuilder2],youtubeLink:undefined,githubLink:"https://github.com/JC33340/CVbuilder.git",technologiesUsed:["react","typescript","HTML","CSS",'tailwind'], Link:'https://cvbuilder2-q33qjdty3-jc33340s-projects.vercel.app/'},
         {name:"Calculator",description:"Basic calculator app",pictures:[calculator1],youtubeLink:undefined,githubLink:"https://github.com/JC33340/CalculatorApp.git",technologiesUsed:["react","typescript","HTML","CSS"],Link:'https://calculatorapp-cwq.pages.dev'},
         {name:"PR tracker",description:"Enter and keep track of PR progress in the gym",pictures:[PRtracker1,PRtracker2],youtubeLink:"https://youtu.be/PSEs7yLWNes",githubLink:"https://github.com/code50/137620817/tree/ec2e1756914286afef16542258eeb663cd85a442/final_project",technologiesUsed:["flask","HTML","CSS","javascript"]},
         {name:"E-mail Replica",description:"A single page replica of an Email application",pictures:[mail1,mail2],youtubeLink:"https://youtu.be/gNSodzbtWhM",githubLink:"https://github.com/JC33340/mail.git",technologiesUsed:["django","HTML","CSS","SQLite"]},
